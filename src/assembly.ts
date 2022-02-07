36438462de504fd329b2176ad6f7ea00d51d27fe
CatsMiaow <infiltrator@naver.com>
2022-02-07 13:15:28 +0900

    Update deps, remove wasi

--- src/assembly.ts
+++ src/assembly.ts
@@ -1,31 +1,17 @@
 /**
+ * https://nodejs.dev/learn/nodejs-with-webassembly
  * https://www.assemblyscript.org/quick-start.html
- * https://docs.wasmer.io/integrations/js/wasi/server/examples/hello-world
  */
 import * as fs from 'fs';
-import { WASI } from '@wasmer/wasi';
-import nodeBindings from '@wasmer/wasi/lib/bindings/node';
 
 import type * as WasmModule from '../addons/optimized.wasm';
-
 type WasmModule = typeof WasmModule & WebAssembly.Exports;
 
 const wasmFilePath = './addons/optimized.wasm';
-const wasi = new WASI({
-  args: [wasmFilePath],
-  env: {},
-  bindings: { ...nodeBindings, fs },
-});
 
-export async function startWasiTask(): Promise<WasmModule> {
-  const wasmBytes = new Uint8Array(fs.readFileSync(wasmFilePath)).buffer;
-  const wasmModule = await WebAssembly.compile(wasmBytes);
-  const instance = await WebAssembly.instantiate(wasmModule, {
-    // Error: Can't detect a WASI namespace for the WebAssembly Module
-    // ...wasi.getImports(wasmModule),
-  });
-
-  wasi.start(instance);
+export async function getWasmModule(): Promise<WasmModule> {
+  const wasmBuffer = fs.readFileSync(wasmFilePath);
+  const { instance } = await WebAssembly.instantiate(wasmBuffer);
 
   return <WasmModule>instance.exports;
 }
